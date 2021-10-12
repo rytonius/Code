c7ef825a47bed8fedb24072b75e689ebf0e7ec7e
Ryan Fuller <ryan3rdeye@gmail.com>
2021-10-12 15:44:26 -0800

    upload wpf C

---
+++ cSharp/The_PSSR/CsharpGUI-PSscriptrunner/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CsharpGUI_PSscriptrunner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_Run_Click(object sender, RoutedEventArgs e)
+        {
+            string path = Directory.GetCurrentDirectory();
+            var ps1File = @"C:\Users\ryan3\OneDrive\Documents\GitHub\Code\cSharp\The_PSSR\CsharpGUI-PSscriptrunner\PSSCripts\testscript.ps1";
+            var startInfo = new ProcessStartInfo()
+            {
+                FileName = "powershell.exe",
+                Arguments = $"-NoProfile -ExecutionPolicy unrestricted -file \"{ps1File}\"",
+                UseShellExecute = false
+            };
+            Process.Start(startInfo);
+            
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
