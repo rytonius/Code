2d962be691842413e5c13d3e5bf5d235ed33019e
Ryan <rfuller@lmjconsulting.com>
2022-02-08 00:30:27 -0900

    Update regex.py

--- Python/Environments/regex.py
+++ Python/Environments/regex.py
@@ -1,5 +1,8 @@
 # https://www.jetbrains.com/help/objc/regular-expression-syntax-reference.html
 # https://regexr.com/
+# https://docs.python.org/3/howto/regex.html
+# https://docs.python.org/3/library/re.html
+# https://docs.python.org/3/howto/regex.html#greedy-versus-non-greedy
 import re
 result = re.search(r"aza","plaza")
 print(result)
@@ -101,3 +104,45 @@ print(check_sentence("is this is a sentence?")) # False
 print(check_sentence("Hello")) # False
 print(check_sentence("1-2-3-GO!")) # False
 print(check_sentence("A star is born.")) # True
+
+
+def check_time(text):
+  pattern = r"^1[0-2]|[1-9]:[0-5][0-9] ?[pA][mM]"
+  result = re.search(pattern, text)
+  return result != None
+
+print(check_time("12:45pm")) # True
+print(check_time("9:59 AM")) # True
+print(check_time("6:60am")) # False
+print(check_time("five o'clock")) # False
+
+def check_zip_code (text):
+  result = re.search(r" \d{5}(?:-\d{4})?", text)
+  return result != None
+
+print(check_zip_code("The zip codes for New York are 10001 thru 11104.")) # True
+print(check_zip_code("90210 is a TV show")) # False
+print(check_zip_code("Their address is: 123 Main Street, Anytown, AZ 85258-0001.")) # True
+print(check_zip_code("The Parliament of Canada is at 111 Wellington St, Ottawa, ON K1A0A9.")) # False
+
+def contains_acronym(text):
+  pattern = r"\(\w.*\w\)"
+  result = re.search(pattern, text)
+  return result != None
+
+print(contains_acronym("Instant messaging (IM) is a set of communication technologies used for text-based communication")) # True
+print(contains_acronym("American Standard Code for Information Interchange (ASCII) is a character encoding standard for electronic communication")) # True
+print(contains_acronym("Please do NOT enter without permission!")) # False
+print(contains_acronym("PostScript is a fourth-generation programming language (4GL)")) # True
+print(contains_acronym("Have fun using a self-contained underwater breathing apparatus (Scuba)!")) # True
+
+def check_web_address(text):
+  pattern = r"\.[a-zA-Z]*$"
+  result = re.search(pattern, text)
+  return result != None
+
+print(check_web_address("gmail.com")) # True
+print(check_web_address("www@google")) # False
+print(check_web_address("www.Coursera.org")) # True
+print(check_web_address("web-address.com/homepage")) # False
+print(check_web_address("My_Favorite-Blog.US")) # True
