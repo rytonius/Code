e318f1cdc53a7e51ed9ab3ea14137560a50ede8f
rytonius <ryan3rdeye@gmail.com>
2022-11-02 23:39:55 -0800

    arrayparams

---
+++ cSharp/LinuxCSharp/ArrayParams/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+namespace ArrayLesson
+{
+    class Arrays
+    {
+        static void Main()
+        {
+            int first = 1, second = 2, third = 3;
+            int[] intArray = { first, second, third };
+            MyArrayClass MAC = new MyArrayClass();
+
+            Console.WriteLine($"\nIntArray starts with: {intArray[0]} {intArray[1]} {intArray[2]}\n");
+            MAC.ListInts(intArray);
+            Console.WriteLine($"IntArray ends with:\t {intArray[0]} {intArray[1]} {intArray[2]}");
+            Console.WriteLine("first second third {0} {1} {2}", first, second, third);
+            
+        }
+
+        
+
+    }
+
+    class MyArrayClass {
+        public void ListInts(params int[] inVals)
+        {
+            
+            if ((inVals != null) && (inVals.Length != 0))
+            {
+                Console.WriteLine("For Loop");
+                for (int i = 0; i < inVals.Length; i++)  // Process the array
+                {
+                    inVals[i] = inVals[i] * 10;
+                    Console.WriteLine($"{inVals[i]}");  // display new value
+                }
+                Console.WriteLine("Foreach");
+                foreach (int value in inVals)
+                {
+                    Console.Write($"{value} ");
+                }
+
+                Console.WriteLine("\nArray.Foreach<t>");
+                Array.ForEach(inVals, Console.WriteLine);  // another way to do it
+                Console.WriteLine("");
+            }
+            else 
+                Console.WriteLine("\nnull value\n\n");
+            
+        }
+    }
+
+
+}
--- cSharp/LinuxCSharp/OutputParams/Program.cs
+++ cSharp/LinuxCSharp/OutputParams/Program.cs
@@ -1,28 +1,37 @@
 ﻿namespace OutputParams
 {
-    class MyClass {
+    class MyClass
+    {
         public int Val = 20;    // Initialize field
     }
 
-    class Program {
-        static void MyMethod(out MyClass f1, out int f2) {
+    class Program
+    {
+        static void MyMethod(out MyClass f1, out int f2)
+        {  // every possible path must assign a value to every 
+           // output param before method exit
             f1 = new MyClass(); // Create an object of the class.  since this isn't a reference it is 
                                 // necessary to initalize before return
-            f1.Val =  25;       // Assign to the class field.
-            f2     =  15;       // Assign to the int param.
+            f1.Val = 25;       // Assign to the class field.
+            f2 = 15;       // Assign to the int param.
         }
 
-        static void Main() {
+        static void Main()
+        {
             MyClass a1;
-            int a2 = 10;
-            Console.WriteLine($"Before: a2: {a2}");
+            int a2 = 10;    // since this will be used as an output param, you do NOT need to initialize
+            Console.WriteLine($"Before: a2: {a2}"); // this is just an example to show the change
             MyMethod(out a1, out a2); // Call the method
 
             Console.WriteLine($"after: a1: {a1.Val}\ta2: {a2}");
+
+            // you can admit declared variables entirely when used as out params
+            MyMethod(out MyClass b1, out int b2);
+            Console.WriteLine($"after: b1: {b1.Val}\tb2+5: {b2 + 5}");
         }
     }
 
-    
+
 
 }
 
