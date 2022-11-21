8825012a585e2a78d13d219e033b66063324f7b0
RyanGaming <57922617+LMJ-Ryan@users.noreply.github.com>
2022-11-21 13:02:33 -0900

    DELETING OLD STUFF

---
+++ cSharp/MauiApp1-withoutblazor/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp1_withoutblazor;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ cSharp/MauiApp1-withoutblazor/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApp1_withoutblazor;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
