7c8554fa652215342ebb9eddb5b3a75592b58ce7
RyanGaming <57922617+LMJ-Ryan@users.noreply.github.com>
2022-11-21 20:49:07 -0900

    WPF RPG

---
+++ cSharp/SOSCSRPG/Engine/Models/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Player : Stats
+    {
+        public string? Name { get; set; }
+        public string? CharacterClass { get; set; }
+        public int Level { get; set; }
+        public float XPtillNextLvl { get; set; }
+        
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Endurance { get; set; }
+        public int gold;
+        public string GoldString 
+        { 
+            get { return String.Format("{0:C}", gold);}
+        }
+    }
+
+    public class Stats 
+    {
+        public int HitPoints { get; set; }
+        public int StamPoints { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int Evade { get; set; }
+        public int BonusAccuracy { get; set; }
+
+    }
+
+}
---
+++ cSharp/SOSCSRPG/Engine/ViewModels/GameSession.cs
@@ -0,0 +1,41 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            { // start new game, so make a player
+                CurrentPlayer = new Player()
+                {
+                    Name = "John Snow",
+                    CharacterClass = "Fighter",
+                    Level = 1,
+                    Strength = 2,
+                    Dexterity = 1,
+                    Endurance = 2,
+                    gold = 100_000_000
+
+                };
+
+                CurrentPlayer.HitPoints += 10 + (CurrentPlayer.Endurance * 2);
+                CurrentPlayer.StamPoints += 10 + (CurrentPlayer.Dexterity + CurrentPlayer.Strength);
+                CurrentPlayer.Attack += CurrentPlayer.Strength * 2 + CurrentPlayer.Dexterity;
+                CurrentPlayer.Defense += CurrentPlayer.Dexterity + CurrentPlayer.Endurance * 2;
+                CurrentPlayer.Evade += CurrentPlayer.Dexterity;
+                CurrentPlayer.BonusAccuracy+= CurrentPlayer.Dexterity;
+                CurrentPlayer.XPtillNextLvl = (float)Math.Round((CurrentPlayer.Level * 40f) * (1.1f), 0);
+                
+            }
+
+        }
+    }
+}
